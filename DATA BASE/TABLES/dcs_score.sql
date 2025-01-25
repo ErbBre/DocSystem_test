766c4e75990888799bc336894cc95817160e168e
Mijael <mijael.es.mo@gmail.com>
2025-01-25 17:41:05 -0500

    Se agrega el archivo dcs_score

--- DATA BASE/TABLES/dcs_score.sql
+++ DATA BASE/TABLES/dcs_score.sql
@@ -1,4 +1,6 @@
+-- ===================================================
 -- Tiket R11 - Tabla de calificaciones por concepto
+-- ===================================================
 /*Columnas:
 
 id autoincrementable
@@ -6,16 +8,18 @@ id_type_concept - llave foranea de la tabla dcs_type_score (de esta forma se det
 number_score (valor decimal de la nota asignada, se valida con el sistema de calificaciones asignado al curso para comprobar que este dentro de algun rango establecido en alguna linea)
 columnas de control*/
 
-create table dcs_score(){
-    id int primary key,                                 -- id autoincrementable
-    id_type_concept int references dcs_type_score(id),  -- id_type_concept - llave foranea de la tabla dcs_type_score (de esta
-    number_score decimal(3,2),                          -- valor decimal de la nota asignada, se valida con el sistema de calificaciones asignado al curso para comprobar que este dentro de algun rango establecido en alguna linea)
+create table dcs_score(
+    id                  serial          primary     key,  -- id autoincrementable
+    id_type_concept     int             not null,         -- id_type_concept - llave foranea de la tabla dcs_type_score (de esta
+    number_score        decimal(3,2)    not null,         -- valor decimal de la nota asignada, se valida con el sistema de calificaciones asignado al curso para comprobar que este dentro de algun rango establecido en alguna linea)
 
     -- Columnas de control 
 
-    us_creation_date timestamp default current_timestamp,
-    us_creation_user varchar(15) default 'user.test',
-    us_update_date timestamp default current_timestamp,
-    us_update_user varchar(15) default 'user.test'
+    us_creation_date    timestamp       default     current_timestamp,
+    us_creation_user    varchar(15)     default     'user.test',
+    us_update_date      timestamp       default     current_timestamp,
+    us_update_user      varchar(15)     default     'user.test',
 
-}   
+    -- Foreign keys
+    foreign key (id_type_concept) references dcs_type_score(id) 
+)

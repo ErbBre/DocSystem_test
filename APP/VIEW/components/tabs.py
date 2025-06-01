247074174de2e14ef202ae1a4d9151228f6002d3
Breyner <breyimp@gmail.com>
2025-06-01 00:26:52 -0500

    New file for tabs managment

---
+++ APP/VIEW/components/tabs.py
@@ -0,0 +1,14 @@
+# PENDIENTE DE TERMINAR... ESTO SERA UN GESTOR DE PESTAÑAS
+from tkinter import *
+
+
+class Tab():
+    def __init__(self,frame,title):
+        pass
+
+v_title = "DCSystem"
+v_message = "Actualizado correctamente"
+
+root = Tk()
+Message=Tab(root, v_title,v_message,"warning")
+root.mainloop()
